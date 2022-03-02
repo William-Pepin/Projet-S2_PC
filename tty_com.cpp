df462c5f47a0cb7a544ca9060a7a1e8920527136
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2022-03-01 19:48:18 -0500

    avant nouvelle source

--- tty_com.cpp
+++ tty_com.cpp
@@ -2,6 +2,9 @@
 //La majorité du code vient de ChristophK sur StackOverflow.
 //Voir: https://stackoverflow.com/questions/32522475/read-write-on-serial-device-dev-ttygs0-with-standard-c-fstream#35749399
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
 #include <fstream>
 #include <iostream>
 #include <thread>
@@ -104,29 +107,100 @@ std::filebuf::int_type FilebufNoSeek::overflow(std::filebuf::int_type __c)
 
 int main()
 {
+    int serial_port = open("/dev/ttyACM0", O_RDWR);
 
-std::cout << "Ouverture du port tty..." << std::endl;
-FilebufNoSeek serialBuf;
-serialBuf.open("/dev/ttyACM0", std::ios_base::out | std::ios_base::in);
+
+    struct termios tty;
+
+  tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+  tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+  tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size
+  tty.c_cflag |= CS8; // 8 bits per byte (most common)
+  tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+  tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+  tty.c_lflag &= ~ICANON;
+  tty.c_lflag &= ~ECHO; // Disable echo
+  tty.c_lflag &= ~ECHOE; // Disable erasure
+  tty.c_lflag &= ~ECHONL; // Disable new-line echo
+  tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+  tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+
+  tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+  tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+  // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
+  // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
+
+  tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+  tty.c_cc[VMIN] = 0;
+
+  // Set in/out baud rate to be 9600
+  cfsetispeed(&tty, B9600);
+  cfsetospeed(&tty, B9600);
+
+  // Save tty settings, also checking for error
+  if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+      return 1;
+  }
+
+    std::cout << "Ouverture du port tty..." << std::endl;
+    FilebufNoSeek serialBuf;
+    serialBuf.open("/dev/ttyACM0", std::ios_base::out | std::ios_base::in);
+
 
 std::cout << "Écriture dans le port tty..." << std::endl;
 std::iostream serialStream(&serialBuf);
 serialStream << "test tty arduino" << std::endl;
 
+std::this_thread::sleep_for(std::chrono::milliseconds(30));
+
 std::cout << "Lecture du port tty..." << std::endl;
+/*
 int i=0;
 int ssize = 0;
-char retour[256];
-while(i < 5000){
-    serialStream.readsome(retour, 255);
-    ssize = serialStream.gcount();
-    retour[ssize] = '\0';
+std::string retour;
+while(i < 10000){
+    //serialStream.sync();
+    //serialStream.seekg(0, serialStream.beg);
+    getline(serialStream,retour);
+    //serialStream.seekg(0, serialStream.beg);
+    //serialStream << std::endl;
+    //ssize = serialStream.gcount();
+    //retour[ssize] = '\0';
     std::cout << retour;
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    std::this_thread::sleep_for(std::chrono::microseconds(100));
     i++;
 }
+*/
 
-serialBuf.close();
 
-return 1;
+    // get length of file:
+int i=0;
+while(i < 5000){
+    serialStream.seekg (0, serialStream.end);
+    int length = serialStream.tellg();
+    serialStream.seekg (0, serialStream.beg);
+
+    if(length != -1){
+    char* buffer = new char [length+1];
+
+    std::cout << "Reading " << length << " characters... ";
+    // read data as a block:
+    serialStream.read (buffer,length);
+
+    buffer[length] = '\0';
+    std::cout << buffer;
+    delete[] buffer;
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    i++;
+
+}
+
+    serialBuf.close();
+    close(serial_port);
+
+return 0;
 }
