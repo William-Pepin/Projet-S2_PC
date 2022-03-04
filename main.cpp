8ee38b5d11ac85efa30a612d08e7e28542ccf54b
William Pepin <wpepin00@gmail.com>
2022-03-04 14:25:40 -0500

    Render the player

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    // create a scene
+    QGraphicsScene * scene = new QGraphicsScene();
+
+    // create an item to put into the scene
+    QGraphicsRectItem * player = new QGraphicsRectItem();
+    player->setRect(0,0,100,100);
+
+    // add the item
+    scene->addItem(player);
+
+    // add a view to visualize the scene
+    QGraphicsView * view = new QGraphicsView();
+
+    // set the scene to the view
+    view->setScene(scene);
+
+    // invisible by default
+    view->show();
+
     return a.exec();
 }
