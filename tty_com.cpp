9e476bc1beacce7cc8027d0c3f97a7747781db38
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2022-03-24 02:10:58 -0400

    Inclusion JSON

--- tty_com.cpp
+++ tty_com.cpp
@@ -1,131 +1,119 @@
-// Autre version du driver pour le tty tiré de et modifiée:
-// https://blog.mbedded.ninja/programming/operating-systems/linux/linux-serial-ports-using-c-cpp
+/* 
+ * Auteurs: Jean-Samuel Lauzon    
+ * Date: Fevrier 2022
+ * Projet S2ei
+*/
 
-// C library headers
-#include <stdio.h>
-#include <string.h>
-
-//C++ library headers
+/*------------------------------ Librairies ---------------------------------*/
 #include <iostream>
-#include <thread>
-#include <chrono>
+#include <string>
+using namespace std;
 
-// Linux headers
-#include <fcntl.h> // Contains file controls like O_RDWR
-#include <errno.h> // Error integer and strerror() function
-#include <termios.h> // Contains POSIX terminal control definitions
-#include <unistd.h> // write(), read(), close()
+/*-------------------------- Librairies externes ----------------------------*/
+#include "include/serial/SerialPort.hpp"
 
-
-//QT headers
-#include <QJsonObject>
-#include <QJsonDocument>
-#include <QString>
+/*------------------------------ Constantes ---------------------------------*/
+#define BAUD 9600           // Frequence de transmission serielle
+#define MSG_MAX_SIZE 1024   // Longueur maximale d'un message
 
 
+/*------------------------- Prototypes de fonctions -------------------------*/
+bool SendToSerial(SerialPort *arduino, json j_msg);
+bool RcvFromSerial(SerialPort *arduino, string &msg);
+
+
+/*---------------------------- Variables globales ---------------------------*/
+SerialPort * arduino; //doit etre un objet global!
+
+/*----------------------------- Fonction "Main" -----------------------------*/
 int main() {
-  QJsonObject json_obj;
-  json_obj["bargraph"] = 5;
+    string raw_msg;
 
-  QJsonDocument json_doc(json_obj);
+    // Initialisation du port de communication
+    string com;
+    cout << "Entrer le port de communication du Arduino: ";
+    cin >> com;
+    arduino = new SerialPort(com.c_str(), BAUD);
+    
+    if(!arduino->isConnected()) {
+        cerr << "Impossible de se connecter au port "<< string(com) <<". Fermeture du programme!" <<endl;
+        exit(1);
+    }
+    
+    // Structure de donnees JSON pour envoie et reception
+    int led_state = 1;
+    int pot_value = 0;
+    json j_msg_send, j_msg_rcv;
 
-  QString json_string = json_doc.toJson();
+    // Boucle infinie pour la communication bidirectionnelle Arduino-PC
+    while(1) {
+        j_msg_send["led"] = led_state;      // Création du message à envoyer
 
-  std::string msg_string = json_string.toStdString();
+        if(!SendToSerial(arduino, j_msg_send)) {    //Envoie au Arduino
+            cerr << "Erreur lors de l'envoie du message. " << endl;
+        }
 
-  char c_msg_string[100];
-  
-  strcpy(c_msg_string, msg_string.c_str());
+        // Reception message Arduino
+        j_msg_rcv.clear(); // effacer le message precedent
+        if(!RcvFromSerial(arduino, raw_msg)) {
+            cerr << "Erreur lors de la reception du message. " << endl;
+        }
+        
+        // Impression du message de l'Arduino, si valide
+        if(raw_msg.size()>0) {
+             j_msg_rcv = json::parse(raw_msg);       // Transfert du message en json
+            pot_value = j_msg_rcv["analog"];        // Transfert dans la variable pot_value
+            cout << "Message de l'Arduino: " << j_msg_rcv << endl;
+        }
+        
+        led_state = !led_state;     //Changement de l'etat led
 
-  int msg_length = strlen(c_msg_string);
-
-  std::cout << "length : " << msg_length << std::endl;
-
-  c_msg_string[msg_length] = '\r';
-
-  // Open the serial port. Change device path as needed (currently set to an standard FTDI USB-UART cable type device)
-  int serial_port = open("/dev/ttyACM0", O_RDWR);
-
-  // Create new termios struct, we call it 'tty' for convention
-  struct termios tty;
-
-  // Read in existing settings, and handle any error
-  if(tcgetattr(serial_port, &tty) != 0) {
-      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
-      return 1;
-  }
-
-  tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
-  tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
-  tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size
-  tty.c_cflag |= CS8; // 8 bits per byte (most common)
-  tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
-  tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
-
-  tty.c_lflag &= ~ICANON;
-  tty.c_lflag &= ~ECHO; // Disable echo
-  tty.c_lflag &= ~ECHOE; // Disable erasure
-  tty.c_lflag &= ~ECHONL; // Disable new-line echo
-  tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
-  tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
-  tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
-
-  tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-  tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-  // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
-  // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
-
-  tty.c_cc[VTIME] = 50;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
-  tty.c_cc[VMIN] = 0;
-
-  // Set in/out baud rate to be 9600
-  cfsetispeed(&tty, B9600);
-  cfsetospeed(&tty, B9600);
-
-  // Save tty settings, also checking for error
-  if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
-      printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
-      return 1;
-  }
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
-while(true){
-  // Write to serial port
-  char msg[] = { 'T', 'e', 's', 't', '\n', '\r' };
-
-  write(serial_port, msg, sizeof(msg));
-  //write(serial_port, c_msg_string, msg_length);
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
-  // Allocate memory for read buffer, set size according to your needs
-  char read_buf[256];
-  std::string read_buffer;
-
-  // Read bytes. The behaviour of read() (e.g. does it block?,
-  // how long does it block for?) depends on the configuration
-  // settings above, specifically VMIN and VTIME
-  
-  int num_bytes = read(serial_port, &read_buf, sizeof(read_buf));
-
-  // n is the number of bytes read. n may be 0 if no bytes were received, and can also be -1 to signal an error.
-  if (num_bytes < 0) {
-      printf("Error reading: %s", strerror(errno));
-      return 1;
-  }
-  read_buffer = read_buf;
-  std::cout << read_buffer;
-  
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(50));
-  return 0;
+        // Bloquer le fil pour environ 1 sec
+        Sleep(1000); // 1000ms
+    }
+    return 0;
 }
-/*
-std::cout << "Lecture du tty..." << std::endl;
-  printf("Read %i bytes. Received message: %s", num_bytes, read_buf);
 
-  close(serial_port);
-  return 0; // success
-  */
-};
+
+/*---------------------------Definition de fonctions ------------------------
+Fonction d'envoi
+Entrée : Port de communication, tampon de réception du message
+Sortie : Code d'erreur, 1 = erreur, 0 = ok
+Traitement : Réception du message
+-----------------------------------------------------------------------------*/
+bool SendToSerial(SerialPort *arduino, json j_msg) {
+    // Return 0 if error
+    string msg = j_msg.dump();
+    bool ret = arduino->writeSerialPort(msg.c_str(), msg.length());
+    return ret;
+}
+
+
+/*---------------------------Definition de fonctions ------------------------
+Fonction de réception
+Entrée : Port de communication, message à envoyer
+Sortie : Code d'erreur, 1 = erreur, 0 = ok
+Traitement : Envoie du message
+-----------------------------------------------------------------------------*/
+bool RcvFromSerial(SerialPort *arduino, string &msg) {
+    // Return 0 if error
+    // Message output in msg
+    string str_buffer;
+    char char_buffer[MSG_MAX_SIZE];
+    int buffer_size;
+
+    msg.clear(); // clear string
+    // Read serialport until '\n' character (Blocking)
+    while(msg.back()!='\n') {
+        if(msg.size()>MSG_MAX_SIZE) {
+            return false;
+        }
+
+        buffer_size = arduino->readSerialPort(char_buffer, MSG_MAX_SIZE);
+        str_buffer.assign(char_buffer, buffer_size);
+        msg.append(str_buffer);
+    }
+
+    msg.pop_back(); //remove '/n' from string
+    return true;
+}
